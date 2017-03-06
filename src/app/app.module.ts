f55964f216a1b6da9e65f40e2270588c4a2d6ff3
Tracy Lee <tracy@thisdot.co>
2017-03-06 15:46:34 -0800

    first-run

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+
+import { FrameworksComponent } from './frameworks/frameworks.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameworksComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    MaterialModule.forRoot(),
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/frameworks/frameworks.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frameworks',
+  templateUrl: './frameworks.component.html',
+  styleUrls: ['./frameworks.component.css']
+})
+export class FrameworksComponent implements OnInit {
+
+  frameworks = [{
+    name: 'Angular',
+    image: 'angular.png'
+  },{
+    name: 'Ember',
+    image: 'ember.png'
+  },{
+    name: 'React',
+    image: 'react.png'
+  },{
+    name: 'RxJS',
+    image: 'rxjs.png'
+  },{
+    name: 'Polymer',
+    image: 'polymer.png'
+  }];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
