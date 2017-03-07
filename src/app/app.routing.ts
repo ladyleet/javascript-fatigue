67fb378b292dc11083be8db342cc0c5be43e3cda
Tracy Lee <tracy@thisdot.co>
2017-03-06 23:01:42 -0800

    nice morning run with some smoked salmon

---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AboutComponent } from './about/about.component';
+import { FrameworksComponent } from './frameworks/frameworks.component';
+
+const appRoutes: Routes = [
+    { path: '', component: FrameworksComponent },
+    { path: 'frameworks', component: FrameworksComponent }
+    ,
+    { path: 'about', component: AboutComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes) ;
